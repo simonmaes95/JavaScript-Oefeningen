29c5c0516f79e81b349a077e66b11f66af93b48e
Simon Maes <simonmaes95@gmail.com>
2024-10-08 12:32:20 +0200

    oefening aangepast

--- 5 Functies/5.2.4 Oefeningen Returnwaarden/oefening4.js
+++ 5 Functies/5.2.4 Oefeningen Returnwaarden/oefening4.js
@@ -5,7 +5,7 @@ function isPrime(n) {
         return false;
     }
 
-    for (let i = 2; i <= Math.sqrt(n); i++) {
+    for (let i = 2; i < n; i++) {
         if (n % i === 0) {
             return false;
         }
