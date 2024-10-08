e6244bfd56424f3f3ee97efbf54135a856a6d666
Simon Maes <simonmaes95@gmail.com>
2024-10-08 11:34:06 +0200

    Oefening 1 dom aangepast

--- 9 DOM-Manipulatie v2/9.4 Oefeningen DOM/Oefening 1/script.js
+++ 9 DOM-Manipulatie v2/9.4 Oefeningen DOM/Oefening 1/script.js
@@ -1,5 +1,4 @@
-let button = document.querySelector("button");
-
+/* Versie 1
 button.addEventListener('click', () => {
     if (button.innerHTML === "Subscribe") {
         button.innerHTML = "Subscribed"
@@ -7,3 +6,19 @@ button.addEventListener('click', () => {
         button.innerHTML = "Subscribe"
     }
 });
+*/
+
+/* Versie 2 */
+let button = document.querySelector("button");
+
+// Let op! De functie subcribe wordt als parameter
+// meegegeven zonder haken!
+button.addEventListener('click', subscribe);
+
+function subscribe() {
+    if (button.innerHTML === "Subscribe") {
+        button.innerHTML = "Subscribed"
+    } else {
+        button.innerHTML = "Subscribe"
+    }
+}
