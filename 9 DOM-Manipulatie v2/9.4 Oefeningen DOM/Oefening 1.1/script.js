ca4c677cd040ffd44faeceadf907b4b1c5c4d2d5
Simon Maes <simonmaes95@gmail.com>
2024-10-14 16:03:07 +0200

    Oefening DOM en Event gesplitst

---
+++ 9 DOM-Manipulatie v2/9.4 Oefeningen DOM/Oefening 1.1/script.js
@@ -0,0 +1,5 @@
+let button = document.createElement('button');
+button.innerHTML = 'Click me!';
+document.body.appendChild(button);
+
+document.title = 'Append Button';
--- test.js
+++ test.js
@@ -1,26 +1,7 @@
-const prompt = require("prompt-sync")();
+let getal = 5;
 
-let aantalLijnen = Number(prompt("Hoeveel lijnen wil je afdrukken? "));
-let regel = "";
-
-for (let i = 0; i < aantalLijnen; i++) {
-	if (i % 2 == 0) {
-		for (let j = 0; j < aantalLijnen; j++) {
-			if (j % 2 == 0) {
-				regel += " ";
-			} else {
-				regel += "#";
-			}
-		}
-	} else {
-		for (let j = 0; j < aantalLijnen; j++) {
-			if (j % 2 == 0) {
-				regel += "#";
-			} else {
-				regel += " ";
-			}
-		}
-	}
-	console.log(regel);
-	regel = "";
+if (getal > 10) {
+	// 
+	// 
+	//
 }
